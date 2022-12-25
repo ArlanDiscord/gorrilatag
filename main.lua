9d74d0f91c6bb46c0f4e0c51eeba29ba93b42dc8
ArlanDiscord <90557945+ArlanDiscord@users.noreply.github.com>
2022-12-25 21:18:24 +0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,22 +1,18 @@
 --[[
-
 █▀▀▀ █▀▀█ █▀▀█ █▀▀█ ░▀░ █░░ █▀▀█ 　 ▀▀█▀▀ █▀▀█ █▀▀▀ 
 █░▀█ █░░█ █▄▄▀ █▄▄▀ ▀█▀ █░░ █▄▄█ 　 ░░█░░ █▄▄█ █░▀█ 
 ▀▀▀▀ ▀▀▀▀ ▀░▀▀ ▀░▀▀ ▀▀▀ ▀▀▀ ▀░░▀ 　 ░░▀░░ ▀░░▀ ▀▀▀▀
-
 █▀ █▀▀ █▀█ █ █▀█ ▀█▀
 ▄█ █▄▄ █▀▄ █ █▀▀ ░█░
-
 Discord: kp1ners#0722
 YouTube: @kp1ners
-
 copy and execute in cheat:
 ======================================================================================================
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ArlanDiscord/gorrilatag/main/main.lua"))()
 ======================================================================================================
 ]]--
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
-local Window = Library.CreateLib("Gorilla Tag { UI SCRIPT } v.0.0.5", "RJTheme3")
+local Window = Library.CreateLib("Gorilla Tag { UI SCRIPT } v.0.0.6", "RJTheme3")
 local gameid = game.PlaceId
 if(gameid == 8690998110) or (gameid == 8999652322) then
 local Tab = Window:NewTab("Action")
@@ -25,19 +21,7 @@ local Section = Tab:NewSection("Choose Actions:")
 local CreditTab = Window:NewTab("Credits")
 local SectionCredit = CreditTab:NewSection("Discord: тарелка#0722, Youtube: @kp1ners")
 local SectionCredit2 = CreditTab:NewSection("Script made from Kazakhstan :)")
-local SectionCredit3 = CreditTab:NewSection("Script Version: v.0.0.5")
-local colors = {
-	-- Цвет фона у Секций
-    SchemeColor = Color3.fromRGB(128, 0, 0),
-	-- Цвет фона в правой части UI
-	Background = Color3.fromRGB(15,15,15),
-	-- Цвет фона в левой части UI
-    Header = Color3.fromRGB(15,15,15),
-	-- Цвет текста
-    TextColor = Color3.fromRGB(255,255,255),
-	-- Цвет фона у кнопок
-    ElementColor = Color3.fromRGB(20, 20, 20)
-}
+local SectionCredit3 = CreditTab:NewSection("Script Version: v.0.0.6")
 print("[Cheat]: Script Gorilla Tag has been enabled!")
 Section:NewSlider("Speed", "Drag this line for speed", 200, 32, function(s)
 	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
@@ -49,6 +33,10 @@ end)
 
 Section:NewSlider("Jump", "Drag this line for jump", 200, 62, function(a)
 	game.Players.LocalPlayer.Character.Humanoid.JumpPower = a
+end)
+
+Section:NewButton("Hide", "If you click this you hide from lava", function(c)
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-50.8270226, 59.2205582, -103.13546, 0.800073087, -7.30333083e-09, -0.59990257, -1.94295318e-08, 1, -3.80868102e-08, 0.59990257, 4.21280575e-08, 0.800073087)
 end)
 
 Section:NewButton("Reset Settings", "If you click this you reset jump and speed", function(l)
@@ -70,5 +58,5 @@ local SectionError1 = Error:NewSection("You need to join to the Gorrilla Tag!")
 local CreditTab = Window:NewTab("Credits")
 local SectionCredit = CreditTab:NewSection("Discord: тарелка#0722, Youtube: @kp1ners")
 local SectionCredit2 = CreditTab:NewSection("Script made from Kazakhstan :)")
-local SectionCredit3 = CreditTab:NewSection("Script Version: v.0.0.5")
+local SectionCredit3 = CreditTab:NewSection("Script Version: v.0.0.6")
 end
