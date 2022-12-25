2f0bda674bee13443461dbea6be8a2a54694f032
ArlanDiscord <90557945+ArlanDiscord@users.noreply.github.com>
2022-12-25 17:02:50 +0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/ArlanDiscord/gorrilat
 ======================================================================================================
 ]]--
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
-local Window = Library.CreateLib("Gorilla Tag { UI SCRIPT } v.0.0.2", "RJTheme3")
+local Window = Library.CreateLib("Gorilla Tag { UI SCRIPT } v.0.0.4", "RJTheme3")
 local gameid = game.PlaceId
 if(gameid == 8690998110) then
 local Tab = Window:NewTab("Action")
@@ -24,7 +24,8 @@ local Player = game:GetService("Players").LocalPlayer
 local Section = Tab:NewSection("Choose Actions:")
 local CreditTab = Window:NewTab("Credits")
 local SectionCredit = CreditTab:NewSection("Discord: тарелка#0722, Youtube: @kp1ners")
-local SectionCredit1 = CreditTab:NewSection("Script Version: v.0.0.2")
+local SectionCredit2 = CreditTab:NewSection("Script made from Kazakhstan :)")
+local SectionCredit3 = CreditTab:NewSection("Script Version: v.0.0.4")
 local colors = {
 	-- Цвет фона у Секций
     SchemeColor = Color3.fromRGB(128, 0, 0),
@@ -38,18 +39,30 @@ local colors = {
     ElementColor = Color3.fromRGB(20, 20, 20)
 }
 print("[Cheat]: Script Gorilla Tag has been enabled!")
-Section:NewSlider("Speed", "SliderInfo", 200, 32, function(s)
+Section:NewSlider("Speed", "Drag this line for speed", 200, 32, function(s)
 	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
 
-Section:NewSlider("Jump", "SliderInfo", 200, 62, function(a)
-	game.Players.LocalPlayer.Character.Humanoid.JumpHeight = a
+Section:NewSlider("Jump", "Drag this line for jump", 200, 62, function(a)
+	game.Players.LocalPlayer.Character.Humanoid.JumpPower = a
 end)
+
+Section:NewButton("Reset Settings", "If you click this you reset jump and speed", function(l)
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 32
+	game.Players.LocalPlayer.Character.Humanoid.JumpPower = 62
+end)
+
+Section:NewKeybind("Reset Settings [BIND]", "If you press bind you reset jump and speed", Enum.KeyCode.P, function(k)
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 32
+	game.Players.LocalPlayer.Character.Humanoid.JumpPower = 62
+end)
+
 else
 local Error = Window:NewTab("Error")
 local SectionError = Error:NewSection("This place is not supported for this script")
 local SectionError1 = Error:NewSection("You need to join to the Gorrilla Tag!")
 local CreditTab = Window:NewTab("Credits")
 local SectionCredit = CreditTab:NewSection("Discord: тарелка#0722, Youtube: @kp1ners")
-local SectionCredit1 = CreditTab:NewSection("Script Version: v.0.0.2")
+local SectionCredit2 = CreditTab:NewSection("Script made from Kazakhstan :)")
+local SectionCredit3 = CreditTab:NewSection("Script Version: v.0.0.4")
 end
