255316541239edf48ee785ce4a994c950684c727
ArlanDiscord <90557945+ArlanDiscord@users.noreply.github.com>
2022-12-25 16:18:14 +0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/ArlanDiscord/gorrilat
 ======================================================================================================
 ]]--
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
-local Window = Library.CreateLib("Gorilla Tag { UI SCRIPT } v.0.0.1", "RJTheme3")
+local Window = Library.CreateLib("Gorilla Tag { UI SCRIPT } v.0.0.2", "RJTheme3")
 local gameid = game.PlaceId
 if(gameid == 8690998110) then
 local Tab = Window:NewTab("Action")
@@ -24,7 +24,7 @@ local Player = game:GetService("Players").LocalPlayer
 local Section = Tab:NewSection("Choose Actions:")
 local CreditTab = Window:NewTab("Credits")
 local SectionCredit = CreditTab:NewSection("Discord: kp1ners#0722, Youtube: @kp1ners")
-local SectionCredit1 = CreditTab:NewSection("Script Version: v.0.0.1")
+local SectionCredit1 = CreditTab:NewSection("Script Version: v.0.0.2")
 local colors = {
 	-- Цвет фона у Секций
     SchemeColor = Color3.fromRGB(128, 0, 0),
@@ -38,11 +38,11 @@ local colors = {
     ElementColor = Color3.fromRGB(20, 20, 20)
 }
 print("[Cheat]: Script Gorilla Tag has been enabled!")
-Section:NewSlider("Speed", "SliderInfo", 12000, 1450, function(s)
+Section:NewSlider("Speed", "SliderInfo", 200, 32, function(s)
 	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
 
-Section:NewSlider("Jump", "SliderInfo", 30, 3, function(a)
+Section:NewSlider("Jump", "SliderInfo", 200, 62, function(a)
 	game.Players.LocalPlayer.Character.Humanoid.JumpHeight = a
 end)
 else
@@ -51,5 +51,5 @@ local SectionError = Error:NewSection("This place is not supported for this scri
 local SectionError1 = Error:NewSection("You need to join to the Gorrilla Tag!")
 local CreditTab = Window:NewTab("Credits")
 local SectionCredit = CreditTab:NewSection("Discord: kp1ners#0722, Youtube: @kp1ners")
-local SectionCredit1 = CreditTab:NewSection("Script Version: v.0.0.1")
+local SectionCredit1 = CreditTab:NewSection("Script Version: v.0.0.2")
 end
